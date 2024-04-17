d39566ab12bfa8b41b490a7c946527552d563118
Гена Иванов <hena300404@gmail.com>
2024-04-17 03:20:08 +0300

    First123

--- backend/kittygram_backend/settings.py
+++ backend/kittygram_backend/settings.py
@@ -9,7 +9,7 @@ SECRET_KEY = 'django-insecure-cg6*%6d51ef8f#4!r3*$vmxm4)abgjw8mo!4y-q*uq1!4$-89$
 
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['158.160.72.72', '127.0.0.1', 'localhost', 'kiiityyygraaammm.webhop.me']
 
 INSTALLED_APPS = [
     'django.contrib.admin',
@@ -100,6 +100,7 @@ USE_TZ = True
 
 
 STATIC_URL = '/static/'
+STATIC_ROOT = BASE_DIR / 'collected_static'
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
